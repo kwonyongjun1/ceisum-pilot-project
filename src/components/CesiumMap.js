87262922ba84bb6465d3383f738cff7152970114
kwonyongjun <dydwns18011@gmail.com>
2024-02-01 14:53:48 +0900

    Feat: box 추가 기능 개발

--- src/components/CesiumMap.js
+++ src/components/CesiumMap.js
@@ -8,8 +8,10 @@ function CesiumMap(){
     const position_GM = Cesium.Cartesian3.fromDegrees( 126.864444,  37.477778, 500); // korea position 
     const cesiumContainer = useRef();
     const [viewer, setViewer] = useState();
+    const [clickPosition, setClickPosition] = useState({});
+    const [addAssetOnMapHandler, setAddAssetOnMapHandler] = useState();
 
-    const isMapExist = () =>{
+    const isMapExist = () =>{ 
         const cesiumContainerElement = cesiumContainer.current;
         if(cesiumContainerElement){
             const cesiumViewerElement = cesiumContainerElement.querySelector('.cesium-viewer');
@@ -21,12 +23,12 @@ function CesiumMap(){
     }
     
     /**
-     * 
+     * 카메라 이동 
      * @param {Cesium.Viewer} viewer 
      */
-    const flyToGM = (viewer) =>{
+    const flyTo = (viewer, position) =>{
         viewer.camera.flyTo({
-            destination: position_GM,
+            destination: position,
             orientation: {
                 heading: Cesium.Math.toRadians(0), // 선택적으로 회전 각도 지정 가능
                 pitch: Cesium.Math.toRadians(-45), // 선택적으로 기울임 각도 지정 가능
@@ -35,31 +37,66 @@ function CesiumMap(){
         });
     }
 
+
+    /**
+     * box 추가 기능 on/off
+     */
+    const addAssetOnMapOnOff = () =>{
+        if(!!addAssetOnMapHandler){
+            addAssetOnMapHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
+            setAddAssetOnMapHandler(null);
+        }else{
+            const handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+            handler.setInputAction((movement) => {
+                const ray = viewer.camera.getPickRay(movement.position);
+                const position = viewer.scene.globe.pick(ray, viewer.scene);
+                setClickPosition(position);
+                
+                if (Cesium.defined(position)) {
+                // 클릭한 위치에 자산 추가
+                //   const asset = viewer.scene.primitives.add(
+                //     new Cesium.Cesium3DTileset({
+                //       url: Cesium.IonResource.fromAssetId(2432421),
+                //       maximumScreenSpaceError: 16,
+                //       position : position
+                //     })
+                //   );
+                //   asset.position = position;
+
+                    viewer.entities.add({
+                        position: position,
+                        box: {
+                            dimensions: new Cesium.Cartesian3(10, 10, 10),
+                            material: Cesium.Color.RED
+                        }
+                    });
+                }
+            }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
+            setAddAssetOnMapHandler(handler);
+        }
+    }
+
     useEffect(()=>{
         const initCeisum = async() => {
             Cesium.Ion.defaultAccessToken = ionAssetID;
 
+            // viewer 생성(3D 지도)
             let newViewer = new Cesium.Viewer('cesiumContainer', {
                 terrain: Cesium.Terrain.fromWorldTerrain({
                     requestVertexNormals: true, //지형 조명
                     requestWaterMask: true, // 물 효과
-                    fullscreenButton: false, // 풀 스크린 버튼
-                    homeButton: false, //홈 버튼
-                    sceneModePicker: false, //지도 모드 버튼
-                    timeline: false, //시간라인
-                    navigationHelpButton: false, //도움말 버튼
-                    animation: false, //재생 등등 버튼
-                    baseLayerPicker: false, //레이어 선택 버튼
-                    geocoder: false, //주소검색 버튼
                 }),
             })
             
-            // Ion 자산 가져온 후 지도에 반영
+            // Ion 자산에 있는 3D 건물 가져온 후 지도에 반영
             const tileset = await Cesium.Cesium3DTileset.fromIonAssetId(96188);
             tileset.maximumScreenSpaceError = 10; // 높을 수록 성능 up, 해상도 down
             newViewer.scene.primitives.add(tileset);
 
-            flyToGM(newViewer);
+            // 카메라 이동
+            flyTo(newViewer, position_GM);
+
+            // viewer 상태값으로 저장
             setViewer(newViewer);
 
             return () => {
@@ -76,7 +113,12 @@ function CesiumMap(){
 
     return(
         <div>
-            <button onClick={()=>{flyToGM(viewer)}}>광명 이동</button>
+            <div>
+                <label>클릭 지점 x : {clickPosition?.x} y : {clickPosition?.y} z : {clickPosition?.z}</label>
+            </div>
+            <button onClick={()=>{flyTo(viewer, position_GM)}}>광명 이동</button>
+            <button onClick={addAssetOnMapOnOff}>건물 추가 on/off</button>
+
             <div id ="cesiumContainer" ref= {cesiumContainer} style={{width: '80%', height: '80%', margin: 0, padding: 0}}>
             </div>
         </div>
