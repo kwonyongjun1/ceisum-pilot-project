7d6077acc0f7a0a0d7a07aeba77889b040fc1e85
kwonyongjun <dydwns18011@gmail.com>
2024-02-07 16:21:51 +0900

    feat: 지도 중복 추가 방지 기능 개발

--- src/components/CesiumMap.js
+++ src/components/CesiumMap.js
@@ -1,10 +1,22 @@
 import * as Cesium from "cesium";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { create, fromIonAssetId, flyTo } from "../map/viewer"
 import { createEventHandler } from "../map/handler";
 
 function CesiumMap(){
     const position_GM = Cesium.Cartesian3.fromDegrees( 126.864444,  37.477778, 500); // korea position  
+    const cesiumContainer = useRef();
+
+    const isMapExist = () =>{
+        const cesiumContainerElement = cesiumContainer.current;
+        if(cesiumContainerElement){
+            const cesiumViewerElement = cesiumContainerElement.querySelector('.cesium-viewer');
+            if(cesiumViewerElement){
+                return true;
+            }
+        }
+        return false;
+    }
 
     const init = async() => {
         create()
@@ -14,11 +26,13 @@ function CesiumMap(){
     }
 
     useEffect(()=>{
-        init();
+        if(!isMapExist){
+            init();
+        }
     },[]);
 
     return(
-        <div id ="cesiumContainer" style={{width: '80%', height: '80%', margin: 0, padding: 0}}>
+        <div id ="cesiumContainer" ref= {cesiumContainer} style={{width: '80%', height: '80%', margin: 0, padding: 0}}>
         </div>
     )
 }
